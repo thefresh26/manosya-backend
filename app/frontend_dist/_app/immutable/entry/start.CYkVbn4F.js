7671bb3a7a9cd6a50f05ec672fc76a33133c6a74
TheFresh <soporte.tecnico@activosporcolombia.com>
2026-09-22 23:43:53 -0500

    Corrige URL del API en el frontend compilado (apuntaba a localhost)

--- app/frontend_dist/_app/immutable/entry/start.CYkVbn4F.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/DJie3e9i.js";import{x as t}from"../chunks/L-Hagbkh.js";export{t as load_css,r as start};
---
+++ app/frontend_dist/_app/immutable/entry/start.DpdKCU5K.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DVx15Ov2.js";import{x as t}from"../chunks/CYV-o48w.js";export{t as load_css,r as start};
