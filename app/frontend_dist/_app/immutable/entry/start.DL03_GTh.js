029bb2ab969b7a9129f851d0bb7091182bd6ddef
TheFresh <soporte.tecnico@activosporcolombia.com>
2026-09-23 12:02:37 +0000

    Actualizar frontend compilado: animaciones del carrusel, mapa y formulario de registro

--- app/frontend_dist/_app/immutable/entry/start.DL03_GTh.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/DG6LciM_.js";import{x as t}from"../chunks/BCRsPUcN.js";export{t as load_css,r as start};
---
+++ app/frontend_dist/_app/immutable/entry/start.fIAPYU5s.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/Bs8rGP_5.js";import{x as t}from"../chunks/DI7kajuI.js";export{t as load_css,r as start};
