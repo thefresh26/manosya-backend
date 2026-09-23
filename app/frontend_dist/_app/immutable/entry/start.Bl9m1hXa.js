06404e1123d56acdb87df1bc38cade837ff04099
TheFresh <soporte.tecnico@activosporcolombia.com>
2026-09-23 17:05:46 +0000

    Actualiza el build estatico del frontend: sitio responsivo para celular

---
+++ app/frontend_dist/_app/immutable/entry/start.Bl9m1hXa.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/CvuhW0kw.js";import{x as t}from"../chunks/DfOWjNWB.js";export{t as load_css,r as start};
--- app/frontend_dist/_app/immutable/entry/start.ChONmaIM.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/CKpSJWTc.js";import{x as t}from"../chunks/BFoS1KQc.js";export{t as load_css,r as start};
