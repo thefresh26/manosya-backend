01c873a62943e6081092052fd3ed1cf27e98f487
TheFresh <soporte.tecnico@activosporcolombia.com>
2026-09-26 00:17:56 +0000

    Sincroniza frontend_dist: animacion de numeros mas suave en el hero
    Reconstruye el frontend con el fix de contador.js que evita el salto
    brusco cuando los datos del backend llegan tarde.

--- app/frontend_dist/_app/immutable/entry/start.COYCNt9l.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/DOfz-7CW.js";import{x as t}from"../chunks/DxleFxR5.js";export{t as load_css,r as start};
---
+++ app/frontend_dist/_app/immutable/entry/start.Cek_e5ql.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/qZbRX7nk.js";import{x as t}from"../chunks/C2CK57UX.js";export{t as load_css,r as start};
